3954bb3b0078b4044f101b6747710855948220e4
Cory Grant <cory.grant11@gmail.com>
2025-04-22 23:55:54 -0400

    gha build dbc

--- dbc_builder/main.py
+++ dbc_builder/main.py
@@ -4,12 +4,8 @@ import canboard
 base_id = 1648
 
 if __name__ == "__main__":
-    dbs = []
-    dbs.append(canboard.build_db(base_id))
-    
-    for db in dbs:
-        filename = "./dbc/" + db.name + "_" + db.version + ".dbc"
-        with open(filename, "w") as f:
-            f.write(db.as_dbc_string())
-        print(db.name + " v" + db.version + " DBC file created successfully!")
+    db = canboard.build_db(base_id)
+
+    with open("./dbc/" + db.name + "_" + db.version + ".dbc", 'w') as f:
+        f.write(db.as_dbc_string())
     
