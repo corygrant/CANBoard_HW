a36891fd1cffc21b4172b8a2327cb0e38aa85aed
Cory Grant <cory.grant11@gmail.com>
2025-04-23 22:34:48 -0400

    dbc change test

--- dbc_builder/main.py
+++ dbc_builder/main.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     db = canboard.build_db(base_id)
 
     with open("./dbc/" + db.name + "_" + db.version + ".dbc", 'w') as f:
-        f.write(db.as_dbc_string())
+        f.write(db.as_dbc_string()) 
     
